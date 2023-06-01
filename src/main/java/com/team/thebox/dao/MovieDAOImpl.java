3f7124337a4464961327199d86b3a47f089b11a5
hoteaklee <cc18ht@naver.com>
2023-06-01 14:10:36 +0900

    영화 리스트 출력, 상세 페이지 출력, 리뷰 출력, 쓰기 구현

--- src/main/java/com/team/thebox/dao/MovieDAOImpl.java
+++ src/main/java/com/team/thebox/dao/MovieDAOImpl.java
@@ -2,9 +2,11 @@ package com.team.thebox.dao;
 
 import com.team.thebox.model.Movie;
 import com.team.thebox.model.MovieAttach;
+import com.team.thebox.model.MovieReply;
 import com.team.thebox.model.MovieSchedule;
 import com.team.thebox.repository.BookedRepository;
 import com.team.thebox.repository.MovieAttachRepository;
+import com.team.thebox.repository.MovieReplyRepository;
 import com.team.thebox.repository.MovieRepository;
 import com.team.thebox.repository.MovieScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +20,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Repository("movdao")
+@Repository
 public class MovieDAOImpl implements MovieDAO {
     @Autowired
     MovieRepository movieRepository;
     @Autowired
     MovieAttachRepository movieAttachRepository;
+
+    @Autowired
+    MovieReplyRepository movieReplyRepository;
 
     @Autowired
     MovieScheduleRepository movieScheduleRepository;
@@ -41,11 +46,6 @@ public class MovieDAOImpl implements MovieDAO {
         return Math.toIntExact(movieAttachRepository.save(ma).getMovano());
     }
 
-    @Override
-    public Movie selectOneMovie(int movno) {
-        System.out.println("겟" + movieRepository.findById((long)movno).get());
-        return movieRepository.findById((long)movno).get();
-    }
 
     @Override
     public Map<String, Object> selectMovie(int cpg) {
@@ -55,6 +55,40 @@ public class MovieDAOImpl implements MovieDAO {
         movs.put("cntpg", movieRepository.findAll(paging).getTotalPages());
         return movs;
     }
+
+    @Override //now
+    public Map<String, Object> selectMovie() {
+        Map<String, Object> movs = new HashMap<>();
+        movs.put("mlist", movieRepository.findAll() );
+        return movs;
+    }
+
+    @Override
+    public int insertMovieReply(MovieReply reply) {
+        return  Math.toIntExact(movieReplyRepository.save(reply).getMovno() );
+    }
+
+    @Override
+    public List<MovieReply> selectOneMovieReply(int movno) {
+        return movieReplyRepository.findByMovnoOrderByRegdateAsc(movno);
+    }
+
+    @Override //상세보기
+    public Movie selectOneMovie(int movno) {
+        return movieRepository.findById((long) movno).get();
+    }
+//    @Override
+//    public Movie selectOneMovie(int mvno) {
+//        Optional<Movie> optionalMovie = movieRepository.findById(mvno);
+//        if (optionalMovie.isPresent()) {
+//            System.out.println("겟" + optionalMovie.get());
+//            return optionalMovie.get();
+//        } else {
+//            // ID에 해당하는 영화가 존재하지 않을 경우 예외 처리 또는 기본 동작을 수행
+//            // 예를 들어, null을 반환하거나 예외를 던질 수 있습니다.
+//            return null; // 또는 원하는 처리 방식에 따라 적절히 수정
+//        }
+//    }
 
     @Override
     public List<String> selectMovieTitle() {
